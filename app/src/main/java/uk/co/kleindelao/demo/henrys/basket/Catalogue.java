771d624c8c394c6cae4520d90790d53a1a700084
Raimund Klein <770876+Chessray@users.noreply.github.com>
2022-01-08 21:44:16 +0000

    ShoppingProcess combines Catalogue and ShoppingBasket.

--- app/src/main/java/uk/co/kleindelao/demo/henrys/basket/Catalogue.java
+++ app/src/main/java/uk/co/kleindelao/demo/henrys/basket/Catalogue.java
@@ -1,11 +1,15 @@
 package uk.co.kleindelao.demo.henrys.basket;
 
 import java.util.Iterator;
-import java.util.Set;
+import java.util.List;
 
-public record Catalogue(Set<ShoppingItem> availableItems) implements Iterable<ShoppingItem> {
+public record Catalogue(List<ShoppingItem> availableItems) implements Iterable<ShoppingItem> {
     @Override
     public Iterator<ShoppingItem> iterator() {
         return availableItems().iterator();
+    }
+
+    public ShoppingItem getItem(final int index) {
+        return availableItems().get(index);
     }
 }
--- app/src/test/java/uk/co/kleindelao/demo/henrys/basket/ShoppingBasketTest.java
+++ app/src/test/java/uk/co/kleindelao/demo/henrys/basket/ShoppingBasketTest.java
@@ -10,90 +10,97 @@ import static org.assertj.core.api.BDDAssertions.then;
 
 import java.math.BigDecimal;
 import org.assertj.core.internal.bytebuddy.utility.RandomString;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class ShoppingBasketTest {
   private final ShoppingBasket shoppingBasket = new ShoppingBasket();
 
-  @Test
-  void shouldBeInitiallyEmpty() {
-    then(shoppingBasket.getContent()).isEmpty();
+  @Nested
+  class Contents {
+    @Test
+    void shouldBeInitiallyEmpty() {
+      then(shoppingBasket.getContent()).isEmpty();
+    }
+
+    @Test
+    void shouldAddEntryForNewItem() {
+      // Given
+      final var item = new ShoppingItem(RandomString.make(), RandomString.make(), ONE);
+      final var numberOfItems = nextInt(1, 10);
+
+      // When
+      shoppingBasket.addItems(numberOfItems, item);
+
+      // Then
+      then(shoppingBasket.getContent()).containsOnly(entry(item, numberOfItems));
+    }
+
+    @Test
+    void shouldSumUpTwoEntriesForSameItem() {
+      // Given
+      final var item = new ShoppingItem(RandomString.make(), RandomString.make(), ONE);
+      final var initialNumberOfItems = nextInt(1, 10);
+      final var addedNumberOfItems = nextInt(1, 10);
+      shoppingBasket.addItems(initialNumberOfItems, item);
+
+      // When
+      shoppingBasket.addItems(addedNumberOfItems, item);
+
+      // Then
+      then(shoppingBasket.getContent()).containsOnly(
+          entry(item, initialNumberOfItems + addedNumberOfItems));
+    }
+
+    @Test
+    void shouldStoreDifferentItems() {
+      // Given
+      final var item1 = new ShoppingItem(RandomString.make(), RandomString.make(), ONE);
+      final var numberOfItems1 = nextInt(1, 10);
+      final var item2 = new ShoppingItem(RandomString.make(), RandomString.make(), TEN);
+      final var numberOfItems2 = nextInt(1, 10);
+      shoppingBasket.addItems(numberOfItems1, item1);
+
+      // When
+      shoppingBasket.addItems(numberOfItems2, item2);
+
+      // Then
+      then(shoppingBasket.getContent()).containsOnly(entry(item1, numberOfItems1),
+          entry(item2, numberOfItems2));
+    }
   }
 
-  @Test
-  void shouldAddEntryForNewItem() {
-    // Given
-    final var item = new ShoppingItem(RandomString.make(), RandomString.make(), ONE);
-    final var numberOfItems = nextInt(1, 10);
+  @Nested
+  class Total {
+    @Test
+    void shouldAddUpToZeroForEmptyBasket() {
+      // When
+      final var total = shoppingBasket.getTotalPrice();
 
-    // When
-    shoppingBasket.addItems(numberOfItems, item);
+      // Then
+      then(total).isEqualTo(ZERO.setScale(2, HALF_UP));
+    }
 
-    // Then
-    then(shoppingBasket.getContent()).containsOnly(entry(item, numberOfItems));
-  }
+    @Test
+    void shouldAddIndividualPricesForUndiscountedItems() {
+      // Given
+      final var itemPrice1 = ONE;
+      final var item1 = new ShoppingItem(RandomString.make(), RandomString.make(), itemPrice1);
+      final var numberOfItems1 = nextInt(1, 10);
+      final var itemPrice2 = TEN;
+      final var item2 = new ShoppingItem(RandomString.make(), RandomString.make(), itemPrice2);
+      final var numberOfItems2 = nextInt(1, 10);
+      shoppingBasket.addItems(numberOfItems1, item1);
+      shoppingBasket.addItems(numberOfItems2, item2);
 
-  @Test
-  void shouldSumUpTwoEntriesForSameItem() {
-    // Given
-    final var item = new ShoppingItem(RandomString.make(), RandomString.make(), ONE);
-    final var initialNumberOfItems = nextInt(1, 10);
-    final var addedNumberOfItems = nextInt(1, 10);
-    shoppingBasket.addItems(initialNumberOfItems, item);
+      // When
+      final var total = shoppingBasket.getTotalPrice();
 
-    // When
-    shoppingBasket.addItems(addedNumberOfItems, item);
-
-    // Then
-    then(shoppingBasket.getContent()).containsOnly(
-        entry(item, initialNumberOfItems + addedNumberOfItems));
-  }
-
-  @Test
-  void shouldStoreDifferentItems() {
-    // Given
-    final var item1 = new ShoppingItem(RandomString.make(), RandomString.make(), ONE);
-    final var numberOfItems1 = nextInt(1, 10);
-    final var item2 = new ShoppingItem(RandomString.make(), RandomString.make(), TEN);
-    final var numberOfItems2 = nextInt(1, 10);
-    shoppingBasket.addItems(numberOfItems1, item1);
-
-    // When
-    shoppingBasket.addItems(numberOfItems2, item2);
-
-    // Then
-    then(shoppingBasket.getContent()).containsOnly(entry(item1, numberOfItems1),
-        entry(item2, numberOfItems2));
-  }
-
-  @Test
-  void shouldAddUpToZeroForEmptyBasket() {
-    // When
-    final var total = shoppingBasket.getTotalPrice();
-
-    // Then
-    then(total).isEqualTo(ZERO.setScale(2, HALF_UP));
-  }
-
-  @Test
-  void shouldAddIndividualPricesForUndiscountedItems() {
-    // Given
-    final var itemPrice1 = ONE;
-    final var item1 = new ShoppingItem(RandomString.make(), RandomString.make(), itemPrice1);
-    final var numberOfItems1 = nextInt(1, 10);
-    final var itemPrice2 = TEN;
-    final var item2 = new ShoppingItem(RandomString.make(), RandomString.make(), itemPrice2);
-    final var numberOfItems2 = nextInt(1, 10);
-    shoppingBasket.addItems(numberOfItems1, item1);
-    shoppingBasket.addItems(numberOfItems2, item2);
-
-    // When
-    final var total = shoppingBasket.getTotalPrice();
-
-    // Then
-    then(total).hasScaleOf(2)
-               .isEqualTo(itemPrice1.multiply(BigDecimal.valueOf(numberOfItems1))
-                                    .add(itemPrice2.multiply(BigDecimal.valueOf(numberOfItems2)))
-                                    .setScale(2, HALF_UP));
+      // Then
+      then(total).hasScaleOf(2)
+                 .isEqualTo(itemPrice1.multiply(BigDecimal.valueOf(numberOfItems1))
+                                      .add(itemPrice2.multiply(BigDecimal.valueOf(numberOfItems2)))
+                                      .setScale(2, HALF_UP));
+    }
   }
 }
